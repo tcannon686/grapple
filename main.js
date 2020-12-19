ec0d54c5b7099414595f6e899bb161778d1bfd85
Thomas Cannon <tom-cannon@playcannon.com>
2020-12-18 18:45:19 -0800

    Moved gamestate to separate file

--- main.js
+++ main.js
@@ -1,99 +1,22 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.module.min.js'
-import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
+import { GameState, GameStateStack } from './gamestate.js'
 
-const canvas = document.querySelector('#canvas')
-const renderer = new THREE.WebGLRenderer({ canvas })
-
-class GameStateStack
-{
-  constructor () {
-    this.gameStateStack = []
-  }
-
-  push(gameState) {
-    this.gameStateStack.push(gameState)
-  }
-
-  pop(gameState) {
-    return this.gameStateStack.pop()
-  }
-
-  clear() {
-    this.gameStateStack = []
-  }
-
-  jump (gameState) {
-    this.clear()
-    this.push(gameState)
-  }
-
-  peek () {
-    return this.gameStateStack[this.gameStateStack.length-1]
-  }
-}
-
-class GameState
-{
-  constructor () {
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(90, canvas.width/canvas.height, 0.1, 1000)
-    this.controls = new OrbitControls(this.camera, renderer.domElement)
-
-    // define the map
-    const map = []
-    const MapSize = 2
-    const MapHeight = 2
-
-    //const material = new THREE.MeshBasicMaterial({ color: 0x050505 })
-    const material = new THREE.MeshNormalMaterial()
-    for (let x=0; x<MapSize; x++)
-    {
-      map[x] = []
-      for (let y=0; y<MapHeight; y++)
-      {
-        map[x][y] = []
-        for (let z=0; z<MapSize; z++)
-        {
-          map[x][y][z] = Math.floor(Math.random() + 0.5)
-
-          // only add a block if there's a block here
-          if (map[x][y][z] == 1)
-          {
-            const geometry = new THREE.BoxGeometry(1, 1, 1)
-            geometry.translate(x,y,z)
-            this.scene.add(new THREE.Mesh(geometry, material))
-          }
-        }
-      }
-    }
-
-    this.camera.position.z = 2
-  }
-
-  render () {
-    renderer.render(this.scene, this.camera)
-  }
-
-  update () {
-    this.camera.aspect = canvas.width / canvas.height
-    this.camera.updateProjectionMatrix()
-    this.controls.update()
-  }
-}
+export const canvas = document.querySelector('#canvas')
+export const renderer = new THREE.WebGLRenderer({ canvas })
 
 function main () {
-  let gameStateStack = new GameStateStack()
+  const gameStateStack = new GameStateStack()
   gameStateStack.push(new GameState())
 
   function render () {
-    let state = gameStateStack.peek()
+    const state = gameStateStack.peek()
     if (state) {
       state.render()
     }
   }
 
   function update (dt) {
-    let state = gameStateStack.peek()
+    const state = gameStateStack.peek()
     if (state) {
       state.update(dt)
     }
