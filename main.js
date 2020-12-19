4dd7219c849af62096edcab729956a0bf6b09308
groverburger <groverburger696@gmail.com>
2020-12-18 18:32:40 -0800

    don't use capital canvas

--- main.js
+++ main.js
@@ -1,33 +1,102 @@
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r123/three.module.min.js'
+import {OrbitControls} from 'https://threejsfundamentals.org/threejs/resources/threejs/r122/examples/jsm/controls/OrbitControls.js';
 
-function main () {
-  const canvas = document.querySelector('#canvas')
-  const renderer = new THREE.WebGLRenderer({ canvas })
+const canvas = document.querySelector('#canvas')
+const renderer = new THREE.WebGLRenderer({ canvas })
 
-  const scene = new THREE.Scene()
-
-  const fov = 75
-  const aspect = canvas.width / canvas.height
-  const near = 0.1
-  const far = 5
-  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-
-  camera.position.z = 2
-
-  {
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
+class GameStateStack
+{
+  constructor () {
+    this.gameStateStack = []
   }
 
+  push(gameState) {
+    this.gameStateStack.push(gameState)
+  }
+
+  pop(gameState) {
+    return this.gameStateStack.pop()
+  }
+
+  clear() {
+    this.gameStateStack = []
+  }
+
+  jump (gameState) {
+    this.clear()
+    this.push(gameState)
+  }
+
+  peek () {
+    return this.gameStateStack[this.gameStateStack.length-1]
+  }
+}
+
+class GameState
+{
+  constructor () {
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(90, canvas.width/canvas.height, 0.1, 1000)
+    this.controls = new OrbitControls(this.camera, renderer.domElement)
+
+    // define the map
+    const map = []
+    const MapSize = 2
+    const MapHeight = 2
+
+    //const material = new THREE.MeshBasicMaterial({ color: 0x050505 })
+    const material = new THREE.MeshNormalMaterial()
+    for (let x=0; x<MapSize; x++)
+    {
+      map[x] = []
+      for (let y=0; y<MapHeight; y++)
+      {
+        map[x][y] = []
+        for (let z=0; z<MapSize; z++)
+        {
+          map[x][y][z] = Math.floor(Math.random() + 0.5)
+
+          // only add a block if there's a block here
+          if (map[x][y][z] == 1)
+          {
+            const geometry = new THREE.BoxGeometry(1, 1, 1)
+            geometry.translate(x,y,z)
+            this.scene.add(new THREE.Mesh(geometry, material))
+          }
+        }
+      }
+    }
+
+    this.camera.position.z = 2
+  }
+
+  render () {
+    renderer.render(this.scene, this.camera)
+  }
+
+  update () {
+    this.camera.aspect = canvas.width / canvas.height
+    this.camera.updateProjectionMatrix()
+    this.controls.update()
+  }
+}
+
+function main () {
+  let gameStateStack = new GameStateStack()
+  gameStateStack.push(new GameState())
+
   function render () {
-    renderer.render(scene, camera)
+    let state = gameStateStack.peek()
+    if (state) {
+      state.render()
+    }
   }
 
   function update (dt) {
-    /* TODO put stuff here. */
+    let state = gameStateStack.peek()
+    if (state) {
+      state.update(dt)
+    }
   }
 
   let lastTime = performance.now() / 1000.0
@@ -41,9 +110,6 @@ function main () {
     canvas.height = window.innerHeight
 
     renderer.setSize(canvas.width, canvas.height)
-
-    camera.aspect = canvas.width / canvas.height
-    camera.updateProjectionMatrix()
 
     /* Update then render. */
     update(dt)
